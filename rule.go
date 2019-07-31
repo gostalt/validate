e9ade76d926b426505c8a7f1c72ef94a21152cb9
Tom Smith <me@tomm.us>
2019-07-31 21:28:08 +0100

    Add basic rules

--- rule.go
+++ rule.go
@@ -1,15 +1,62 @@
 package validate
 
 import (
+	"fmt"
 	"net/http"
+	"regexp"
 )
 
 // Rule represents a check to run on a request.
 type Rule struct {
-	// Param is the field in the Request to check.
+	// Param is the field in the request to check.
 	Param string
-	// Check is a callback that is ran on the request. The
-	// second argument is the param to check. If the check
-	// fails, an error should be returned with details why.
-	Check func(*http.Request, string) error
+	// Check is a callback that is ran against the request.
+	Check CheckFunc
 }
+
+type CheckFunc func(*http.Request, string) error
+
+var (
+	// Required returns an error if the parameter is not in the request.
+	// Additional checks should be made to ensure it is not empty, etc.
+	Required CheckFunc = func(r *http.Request, param string) error {
+		if _, exists := r.Form[param]; !exists {
+			return fmt.Errorf("%s is required", param)
+		}
+
+		return nil
+	}
+
+	// Alpha returns an error if the parameter contains any characters
+	// that are not in the alphabet, represented by the regular
+	// expression `[a-zA-Z]+`.
+	Alpha CheckFunc = func(r *http.Request, param string) error {
+		fail, _ := regexp.MatchString(`[^a-zA-Z]+`, r.Form.Get(param))
+
+		if fail {
+			return fmt.Errorf("%s must only contain alphabetical characters", param)
+		}
+
+		return nil
+	}
+
+	Alphanumeric CheckFunc = func(r *http.Request, param string) error {
+		fail, _ := regexp.MatchString(`[^a-zA-Z0-9]+`, r.Form.Get(param))
+
+		if fail {
+			return fmt.Errorf("%s must only contain alphanumeric characters", param)
+		}
+
+		return nil
+	}
+
+	Boolean CheckFunc = func(r *http.Request, param string) error {
+		value := r.Form.Get(param)
+
+		if value == "true" || value == "false" || value == "1" || value == "0" {
+			return nil
+		}
+
+		return fmt.Errorf("%s must be a boolean value", param)
+	}
+)
---
+++ rule_test.go
@@ -0,0 +1,74 @@
+package validate
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestRules(t *testing.T) {
+	// Make a dummy request variable.
+	r, _ := http.NewRequest("POST", "localhost", nil)
+	r.ParseForm()
+
+	rules := []struct {
+		Check  CheckFunc
+		Passes []string
+		Fails  []string
+	}{
+		{
+			Alpha,
+			[]string{"Alphabet", "lowercase", "UPPERCASE"},
+			[]string{"Alphab3tic4l", "13567", "letters-and-dashes"},
+		},
+		{
+			Alphanumeric,
+			[]string{"Alphanumeric123", "123alpha", "123", "abc"},
+			[]string{"number-letter-dash", "__", "--"},
+		},
+		{
+			Boolean,
+			[]string{"true", "false", "1", "0"},
+			[]string{"2", "truthy", "falsy"},
+		},
+	}
+
+	for _, rule := range rules {
+		// First, ensure the check passes
+		for _, value := range rule.Passes {
+			r.Form.Set("parameter", value)
+			msgs, _ := Check(r, Rule{"parameter", rule.Check})
+			if len(msgs) > 0 {
+				fmt.Println("Got an error, expected none:", msgs[0].Error)
+				fmt.Println("Value was", value)
+				t.FailNow()
+			}
+		}
+
+		// Then, ensure that it can fail
+		for _, value := range rule.Fails {
+			r.Form.Set("parameter", value)
+			msgs, _ := Check(r, Rule{"parameter", rule.Check})
+			if len(msgs) == 0 {
+				fmt.Println("Expected an error, didn't get one")
+				fmt.Println("Value was", value)
+				t.FailNow()
+			}
+		}
+	}
+}
+
+func TestRequiredRule(t *testing.T) {
+	r, _ := http.NewRequest("GET", "localhost", nil)
+
+	rule := Rule{
+		Param: "anything",
+		Check: Required,
+	}
+
+	msgs, _ := Check(r, rule)
+	if len(msgs) == 0 {
+		fmt.Println("expected an error, didn't get one")
+		t.FailNow()
+	}
+}
